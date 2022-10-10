5be310a96752ff470897dbcb62aa51aa88adaa53
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2022-10-10 12:11:18 +0100

    Refining the Thing model

--- things/models.py
+++ things/models.py
@@ -10,10 +10,12 @@ class Thing(models.Model):
     )
 
     description = models.CharField(
+    unique=False,
     max_length=120,
     blank=True
     )
 
     quantity = models.IntegerField(
+    unique=False,
     validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
